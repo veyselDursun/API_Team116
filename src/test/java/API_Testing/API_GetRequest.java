cdbe0ea9c361debc7fbe4b55f516feddc073fb3a
veyselDursun <veysdursun@yahoo.com>
2023-08-30 20:17:48 +0200

    FirstCommit

--- src/test/java/API_Testing/API_GetRequest.java
+++ src/test/java/API_Testing/API_GetRequest.java
@@ -1,4 +1,41 @@
 package API_Testing;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class API_GetRequest {
+
+
+    @Test
+
+    public void get01(){
+
+        // 1- Request URL ve BODY olustur
+        String url="https://jsonplaceholder.typicode.com/posts/44";
+
+        // 2- Expected Data olustur
+        JSONObject expentedBody = new JSONObject();
+        expentedBody.put("userId",5);
+        expentedBody.put("title","optio dolor molestias sit");
+
+        // 3- Responsu kaydet
+        Response response = given().when().get(url);
+        response.prettyPrint();
+
+        // 4- Assertion
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON);
+
+        JsonPath actualBody = response.jsonPath();
+        Assert.assertEquals(expentedBody.get("userId"),actualBody.get("userId"));
+        Assert.assertEquals(expentedBody.get("title"),actualBody.get("title"));
+    }
 }
---
+++ src/test/java/API_Testing/API_PostRequest.java
@@ -0,0 +1,64 @@
+package API_Testing;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class API_PostRequest {
+
+
+    @Test
+    public void post01(){
+
+        // 1- Request URL ve BODY olustur
+        String url="https://jsonplaceholder.typicode.com/posts";
+        JSONObject regBody = new JSONObject();
+
+        regBody.put("title", "API");
+        regBody.put("body", "API ögrenmek ne güzel");
+        regBody.put("userId", 10);
+
+        // 2- Expected Data olustur
+        JSONObject expBody = new JSONObject();
+
+        expBody.put("title", "API");
+        expBody.put("body", "API ögrenmek ne güzel");
+        expBody.put("userId", 10);
+
+        // 3- Responsu kaydet
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                body(regBody.toString()).
+                post(url);
+
+        JsonPath actBody = response.jsonPath();
+
+        // 4- Assertion
+
+        response.
+                then().
+                assertThat().
+                contentType(ContentType.JSON).
+                statusCode(201);
+
+
+        Assert.assertEquals(expBody.get("title"),actBody.get("title"));
+        Assert.assertEquals(expBody.get("body"),actBody.get("body"));
+        Assert.assertEquals(expBody.get("userId"),actBody.get("userId"));
+
+
+
+
+
+
+
+
+
+    }
+}
