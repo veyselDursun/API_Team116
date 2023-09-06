3ca2e7738c6594305add2a5faffb4af4c9cf9c0d
veyselDursun <veysdursun@yahoo.com>
2023-09-06 12:28:07 +0200

    C17

---
+++ src/test/java/baseURLDeposu/baseURL_JSONHolder.java
@@ -0,0 +1,22 @@
+package baseURLDeposu;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class baseURL_JSONHolder {
+
+    // BaseURL olustururken RequestSpecification interface inden bir specobje olusturduk
+
+    protected RequestSpecification specJSONHolder;
+
+    @Before
+    public void setUP(){
+
+         specJSONHolder = new RequestSpecBuilder().
+                setBaseUri("https://jsonplaceholder.typicode.com").
+                build();
+
+    }
+
+}
